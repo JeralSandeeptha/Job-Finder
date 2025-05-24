653dad2ee140220bd9a433074ed232fcfbeb0b76
Robert Thushidan <robertthushidan@gmail.com>
2025-05-24 23:18:53 +0530

    Searchbar_changes_1.1

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
             <div>Jobs</div>
           </Link>
           <li className=" relative nav-link text-[#000000] font-normal text-[1.2rem]">About us</li>
-          <Link to={'/add/job'} className='post-job bg-[#00773A] py-3 pt-2.5 px-10 font-semibold text-[1.2rem] text-[#FFFFFF] rounded-full flex items-center gap-3 '><CirclePlus className='mt-0.5' />Post a job</Link>
+          <Link to={'/add/job'} className='post-job bg-[#00773A] py-3 pt-2.5 px-10 font-semibold text-[1.2rem] text-[#FFFFFF] rounded-full flex items-center gap-3 hover:bg-[linear-gradient(to_left,#009A4B_1%,#35A44C_1%,#57AB4D_16%,#6EAF4E_51%,#9EB84F_76%,#E5C651_100%)] hover:scale-105 transition-all duration-500 ease-in-out'><CirclePlus className='mt-0.5' />Post a job</Link>
         </ul>
       </div>
     </div>
