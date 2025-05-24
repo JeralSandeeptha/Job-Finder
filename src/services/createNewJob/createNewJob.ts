365f2a22de171bde6c806396b92879989238be16
Pamoodith Maduwantha <99198913+Pamodith@users.noreply.github.com>
2025-05-25 02:26:47 +0530

    post data

--- src/services/createNewJob/createNewJob.ts
+++ src/services/createNewJob/createNewJob.ts
@@ -4,16 +4,11 @@ import { baseURL } from "../api/baseURL";
 
 const createNewJob = async (props: CreateNewJobProps) => {
     try {
-        await axios.get(`${baseURL}/jobs`)
-                .then((res) => {
-                    console.log(res.data);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+        const res = await axios.post(`${baseURL}/jobs`, props.newJob);
+        console.log(res.data);
     } catch (error) {
         console.log(error);
     }
-}
+};
 
 export default createNewJob;
