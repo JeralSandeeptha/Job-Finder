7776a3550942c9296bd66c0034377ea8c292e455
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-05-30 01:01:25 +0530

    Fixed some bugs

--- src/store/provider/AuthProvider.tsx
+++ src/store/provider/AuthProvider.tsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import type { AuthProviderProps } from "../../types/context.types";
 import AuthContext from "../context/AuthContext";
+import useLocalStorage from "../../hooks/usLocalStorage";
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
 
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const { getLocalStorageItem } = useLocalStorage();
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
+        return getLocalStorageItem('isLoggedIn');
+    });
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
